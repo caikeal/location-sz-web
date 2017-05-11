9be567604c4ec2e9b21f2b71358fcd951f0af7a4
caikeal <546487590@qq.com>
2017-05-12 00:03:22 +0800

    巡检任务图片上传控件

--- src/view/task/task-detail.vue
+++ src/view/task/task-detail.vue
@@ -11,6 +11,9 @@
 				title="是否有可疑物品"
 				v-model="suspectiveItem"
 				:options="optionsSi" />
+			<div class="img-upload">
+				<k-img-btn type="card"></k-img-btn>
+			</div>
 			<mt-field placeholder="发现可疑情况，请描述" type="textarea" rows="4" v-model="description"></mt-field>
 		</div>
 		<div class="control-bar">
@@ -23,6 +26,7 @@
 	import changeTitle from '../../utils/changeTitle.js';
 	import { mapState, mapMutations } from 'vuex';
 	import { Radio, Field, Button } from 'mint-ui';
+	import ImgUpload from '../../components/image-upload/image-upload.vue';
 
 	export default {
 		name: 'huawei-task-detail',
@@ -82,18 +86,51 @@
 		components: {
 			[Radio.name]: Radio,
 			[Field.name]: Field,
-			[Button.name]: Button
+			[Button.name]: Button,
+			[ImgUpload.name]: ImgUpload
 		}
 	};
 </script>
 <style lang="scss">
+	@import '../../assets/sass/parital/_var.scss';
 	.huawei-task-detail {
+		.form-task {
+			margin-top: 25px;
+			.mint-radiolist-title {
+				padding: 5px;
+				font-size: 16px;
+				color: $color-black;
+			}
+			.page-part {
+				&:not(:first-child) {
+					background-image: -webkit-linear-gradient(bottom, #d9d9d9, #d9d9d9 50%, transparent 50%);
+					background-image: linear-gradient(0deg, #d9d9d9, #d9d9d9 50%, transparent 50%);
+					background-size: 100% 1px;
+					background-repeat: no-repeat;
+					background-position: top left;
+				}
+				.mint-cell {
+					float: left;
+					.mint-cell-wrapper {
+						background-image: none;
+					}
+					&:last-child {
+						background-image: none;
+					}
+				}
+			}
+			.img-upload {
+			    margin: 10px 15px;
+			}
+		}
 		.control-bar {
-			margin-top: 20px;
+			margin-top: 35px;
 			padding: 0 25px;
 		}
 		.btn-cancel {
 			margin-top: 15px;
+			background-color: $cancel-color;
+			color: $cancel-word;
 		}
 	}
 </style>
